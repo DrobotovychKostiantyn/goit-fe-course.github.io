6d5e4c061667c64b10da87fa9f812d102f7b8d9e
Kostia Drobotovych <k.drobotovych@gmail.com>
2018-07-13 11:39:21 +0300

    fix hw 3

--- javascript/module-03/script.js
+++ javascript/module-03/script.js
@@ -1,35 +1,31 @@
-'use strict'
+"use strict";
 
 const logins = ["Mango", "robotGoogles", "Poly", "Aj4x1sBozz", "qwerty123"];
 
 const checkLoginValidity = login => {
-    let loginSplit = login.split('');
-    let isTrue;
-    for (let i = 0; i < loginSplit.length; i++) {
-      isTrue = i >= 3 && i < 16 ? true : false;
-    }
-    return isTrue;
-}
+  return login.length > 3 && login.length < 17 ? true : false;
+};
 
 const checkIfLoginExists = (logins, login) => {
-    let isIn = logins.includes(login);
-    return isIn ? true : false;
-}
+  let isIn = logins.includes(login);
+  return isIn ? true : false;
+};
 
 const addLogin = (logins, login) => {
-  let validity =  checkLoginValidity(login);
-  if (validity === false) return 'Ошибка! Логин должен быть от 4 до 16 символов';
-  
-  let exists = checkIfLoginExists(logins,login);
+  let validity = checkLoginValidity(login);
+  if (validity === false)
+    return "Ошибка! Логин должен быть от 4 до 16 символов";
+
+  let exists = checkIfLoginExists(logins, login);
   if (exists === false) {
     logins.push(login);
-    return 'Логин успешно добавлен!';
+    return "Логин успешно добавлен!";
   } else {
-    return 'Такой логин уже используется!';
+    return "Такой логин уже используется!";
   }
-}
+};
 
-console.log(addLogin(logins,'Poly'));
+console.log(addLogin(logins, "Poly"));
 
 //массив логинов
-//console.log(logins);
+console.log(logins);
